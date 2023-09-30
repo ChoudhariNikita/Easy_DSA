50830c1df14acd190ad7b0690dae3e3f848d6100
Nikita Choudhari <97299411+ChoudhariNikita@users.noreply.github.com>
2023-10-01 00:29:15 +0530

    Update ContainerWithMostWaterVVI.java with explanation

--- ArrayLists/ContainerWithMostWaterVVI.java
+++ ArrayLists/ContainerWithMostWaterVVI.java
@@ -41,3 +41,26 @@ public class ContainerWithMostWaterVVI {
  *     Output
  *     49
  */
+
+
+/*
+The storeWater method takes an ArrayList of integers height as input and returns an integer representing the maximum water that can be held.
+
+Inside the storeWater method, there are two nested loops. The outer loop (for(int i=0; i<height.size(); i++)) iterates over the lines starting from the first line, and the inner loop (for(int j=i+1; j<height.size(); j++)) iterates over the lines starting from the line immediately to the right of the current outer loop line.
+
+For each pair of lines represented by indices i and j, the code calculates the height of the container, which is the minimum height between the lines (int ht = Math.min(height.get(i), height.get(j))).
+
+It also calculates the width of the container, which is the difference between the indices i and j (int width = j - i).
+
+The code then calculates the current water that can be held by multiplying the height and width of the container (int currWater = ht * width).
+
+The maxWater variable keeps track of the maximum water found so far, and it is updated whenever a larger water volume is encountered (maxWater = Math.max(maxWater, currWater)).
+
+After both loops have finished, the maxWater variable contains the maximum water that can be held between any two lines in the height ArrayList.
+
+In the main method, you create an ArrayList called height and populate it with integer values representing the heights of the lines.
+
+You call the storeWater method with the height ArrayList as an argument and then print the result, which is the maximum water that can be held between any two lines.
+
+The expected output for the provided height ArrayList is 49, which represents the maximum water that can be held between the lines with heights 8 and 7
+*/
